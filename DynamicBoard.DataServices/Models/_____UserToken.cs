9ebac43428410b2d9c0b4d4ca648cd436c04a64e
Ather <atherrehan@live.com>
2025-04-15 07:44:48 +0400

    Removed unused using

--- DynamicBoard.DataServices/Models/_____UserToken.cs
+++ DynamicBoard.DataServices/Models/_____UserToken.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-
-namespace DynamicBoard.DataServices
+﻿namespace DynamicBoard.DataServices
 {
     //public class UserToken
     //{
