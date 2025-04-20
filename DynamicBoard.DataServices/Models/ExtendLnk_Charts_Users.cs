f06b657aa718809a607a31a9d66d2b661fa3a749
Ather <atherrehan@live.com>
2025-04-20 08:03:13 +0400

    Removed unused using

--- DynamicBoard.DataServices/Models/ExtendLnk_Charts_Users.cs
+++ DynamicBoard.DataServices/Models/ExtendLnk_Charts_Users.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DynamicBoard.DataServices.Models
+﻿namespace DynamicBoard.DataServices.Models
 {
     public class ExtendLnk_Charts_Users: Lnk_Charts_Users
     {
